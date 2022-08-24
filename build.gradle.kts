e1c782639734b0167c6a5f5f755211e0c770f72a
Greg Adams <greg@moderne.io>
2022-08-24 09:45:35 -0700

    moderne ast plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,12 @@ buildscript {
 
 plugins {
     java
+    id("io.moderne.rewrite") version("0.21.0")
+    `maven-publish`
 }
 
 group = "org.example"
+version = "1.0.0"
 
 repositories {
     mavenLocal()
@@ -28,4 +31,22 @@ dependencies {
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
     options.compilerArgs.addAll(listOf("-parameters"))
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "artifactory"
+            url = uri("https://artifactory.moderne.ninja/artifactory/greg-local")
+            credentials {
+                username = project.property("artifactory.username") as String
+                password = project.property("artifactory.password") as String
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("moderne") {
+            artifact(tasks["moderneJar"])
+        }
+    }
 }
